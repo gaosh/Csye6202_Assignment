02932f0b7913d7996b93e5e6a5611440d73d8e14
gaosh <gao.sh@husky.neu.edu>
2016-03-26 13:06:14 -0400

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz
+{
+	public class FizzBuzz
+	{
+		public string RunFizzBuzz(int number)
+		{
+			string result = number.ToString();
+
+            if (number == 0)
+            {
+               
+            }
+            else if (number % 15 == 0)
+            {
+                result = "FizzBuzz";
+
+            }
+            else if(number % 3 == 0)
+            {
+                result = "Fizz";
+            }
+            else if(number % 5 == 0)
+            {
+                result = "Buzz";
+            }
+                
+
+
+
+			// your solution/implementation should go in here
+
+			return result;
+		}
+	}
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            FizzBuzz FB = new FizzBuzz();
+            int number;
+            string s = System.Console.ReadLine();
+            Int32.TryParse(s,out number);
+            Console.WriteLine(FB.RunFizzBuzz(number));
+            Console.Read();
+		}
+	}
+}
